ba43e7c08fccf5a48d39012728c6c3ea4e4011c8
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-04-25 16:07:40 +0530

    Update DataComponent.jsx

--- src/components/DataComponent.jsx
+++ src/components/DataComponent.jsx
@@ -1,20 +1,13 @@
-import elephant from "https://github.com/rapolurenu/lab-react-props-boilerplate/blob/main/src/images/elephant.jpeg?raw=true";
-const imgdata = [
-  {
-    id: 1,
-    img: elephant
-  },
-  {
-    id: 2,
-    img: elephant
-  },
-  {
-    id: 3,
-    img: elephant
-  },
-  {
-    id: 4,
-    img: elephant
-  }
-];
-export default imgdata;
+import elephant from '../images/elephant.jpeg'; 
+const DataComponent = () => {
+    const data = [
+        { id: 1, img: elephant },
+        { id: 2, img: elephant },
+        { id: 3, img: elephant },
+        { id: 4, img: elephant }
+    ];
+
+    return data;
+};
+
+export default DataComponent;
