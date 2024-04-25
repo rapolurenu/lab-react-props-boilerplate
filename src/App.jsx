e0c8ba2ab2bca3b3f1dc00c8965970015429f15b
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-04-25 14:51:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
 import './App.css';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
-
 function App() {
   // code here
   const images =imageData();
@@ -37,5 +15,9 @@ function App() {
     </div>
   );
 }
-
+function Props(img_Comp) {
+  return (
+    <img key={img_Comp.id} src={img_Comp.img} alt={`Elephant ${img_Comp.id}`} />
+  );
+}
 export default App;
