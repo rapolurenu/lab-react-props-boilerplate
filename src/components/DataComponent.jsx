4daae3b3fce787e0c07e2e79205455e0bc68263f
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-04-25 14:48:13 +0530

    Update DataComponent.jsx

--- src/components/DataComponent.jsx
+++ src/components/DataComponent.jsx
@@ -1,1 +1,20 @@
-
+import elephant from "../images/elephant.jpeg";
+const data = [
+  {
+    id: 1,
+    img: elephant
+  },
+  {
+    id: 2,
+    img: elephant
+  },
+  {
+    id: 3,
+    img: elephant
+  },
+  {
+    id: 4,
+    img: elephant
+  }
+];
+export default data;
