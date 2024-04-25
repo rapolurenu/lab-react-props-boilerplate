8cfea4e1c9a24b1f663cc5a8cf8f131d2f6d1606
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-04-25 14:54:59 +0530

    Update AppClass.jsx

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -3,29 +3,6 @@ import "./App.css"
 import elephant from "./images/elephant.jpeg";
 
 export default class AppClass extends Component{
-  
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
-  }
-
   // code here
   render(){
     const images = this.imageData();
@@ -38,6 +15,14 @@ export default class AppClass extends Component{
           ))}
         </div>
       </div>
+    );
+  }
+}
+class Props extends Component {
+  render() {
+    const { id, img } = this.img_Comp;
+    return (
+      <img key={id} src={img} alt={`Elephant ${id}`} />
     );
   }
 }
